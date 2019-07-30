e794c2661d19a38718ca22dd5f18e69b8e68578b
Adam Petrovic <apetrovic@atlassian.com>
2019-07-30 11:38:13 +1000

    Various updates

--- install/dock.sh
+++ install/dock.sh
@@ -9,10 +9,9 @@ dockutil --no-restart --add "/Applications/Fantastical 2.app"
 dockutil --no-restart --add "/Applications/iTerm.app"
 dockutil --no-restart --add "/Applications/Visual Studio Code.app"
 dockutil --no-restart --add "/Applications/Things3.app"
-dockutil --no-restart --add "/Applications/Workflowy.app"
 dockutil --no-restart --add "/Applications/Telegram.app"
 dockutil --no-restart --add "/Applications/Spotify.app"
-dockutil --no-restart --add "/Applications/Reeder.app"
+dockutil --no-restart --add "/Applications/Reeder.localized/Reeder.app"
 dockutil --no-restart --add "/Applications/System Preferences.app"
 dockutil --add '/Applications' --view grid --display folder --sort name
 dockutil --add '~/Downloads' --view grid --display folder --sort dateadded

de52d89ff759854d170c75fa3df8ddc55caf0eaf
Adam Petrovic <apetrovic@atlassian.com>
2020-08-06 17:28:00 +1000

    refactor ssh approach to use 1password completely

--- install/ssh-keys.sh
+++ install/ssh-keys.sh
@@ -14,19 +14,28 @@ fi
 case "$PROFILE" in
 "work")
     echo "Downloading Atlassian SSH key from 1password"
-    OP_UUID="g6jq64b6vjgfjgs6s4hmucfrly"
+    OP_UUID=$(op list items | jq -r '.[] | select(.overview.title == "Atlassian - SSH Key") | .uuid')
 
-    rm -f ~/.ssh/id_rsa.atlassian
+    rm -f $HOME/.ssh/id_rsa.atlassian*
     op get document ${OP_UUID} > $HOME/.ssh/id_rsa.atlassian
+    # make sure we have strict permissions on our keyfiles
     chmod 600 $HOME/.ssh/id_rsa.atlassian
+    # generate public key file
+    PASSPHRASE=$(op get item ${OP_UUID} | jq -r '.details.sections[].fields[]? | select(.t == "passphrase") | .v')
+    ssh-keygen -y -P ${PASSPHRASE} -f $HOME/.ssh/id_rsa.atlassian > $HOME/.ssh/id_rsa.atlassian.pub
     ;;
 esac
 
 
-OP_UUID="k4byzx4zizhpnchktv6ojsiw5e"
-rm -f ~/.ssh/id_rsa.personal
+OP_UUID=$(op list items | jq -r '.[] | select(.overview.title == "Personal - SSH Key") | .uuid')
+PASSPHRASE=$(op get item ${OP_UUID} | jq -r '.details.sections[].fields[]? | select(.t == "passphrase") | .v')
+rm -f $HOME/.ssh/id_rsa.personal*
+# save personal ssh key
 op get document ${OP_UUID} > $HOME/.ssh/id_rsa.personal
+# make sure we have strict permissions on our keyfiles
 chmod 600 $HOME/.ssh/id_rsa.personal
+# generate public key file
+ssh-keygen -y -P ${PASSPHRASE} -f $HOME/.ssh/id_rsa.personal > $HOME/.ssh/id_rsa.personal.pub
 
 # Finished
 echo "$(basename "$0") complete."
